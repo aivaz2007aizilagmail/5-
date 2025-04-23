e6bbf53a2c5a3df9c6de7c654bd931a21c770a57
Aivaz <105557584+aivaz2007aizilagmail@users.noreply.github.com>
2025-04-23 14:22:52 +0300

    Update wirez.c

--- wirez.c
+++ wirez.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-void extract_substring(const char* src, char* dest, int start, int length) {
-    for (int i = 0; i < length && src[start + i] != '\0'; i++) {
-        dest[i] = src[start + i];
-    }
-    dest[length] = '\0';
-}
-
 int main() {
-    const char* word = "yellow";
-    int start_pos = 3; // Начало подстроки (индексация с 0)
-    int sub_length = 3; // Длина подстроки "low"
+    char original[] = "yellow";
+    char new_str[4];
+    int from = 3;
+    int to = 6;
 
-    // Выделяем память для подстроки + терминатор
-    char substring[sub_length + 1];
-    
-    extract_substring(word, substring, start_pos, sub_length);
-    
-    printf("Исходная строка: %s\n", word);
-    printf("Извлеченная подстрока: %s\n", substring);
+    int new_idx = 0;
+    for (int old_idx = from; old_idx < to; old_idx++) {
+        new_str[new_idx] = original[old_idx];
+        new_idx++;
+    }
+    new_str[new_idx] = '\0';
+
+    printf("Результат: %s\n", new_str);
 
     return 0;
 }
